4acb2a8230366027810e4374a361eff1002ee9fb
logapriyanm <logapriyanvky@gmail.com>
2025-09-16 20:55:08 +0530

    URL Chnaged

--- backend/server.js
+++ backend/server.js
@@ -8,13 +8,13 @@ dotenv.config();
 const app = express();
 app.use(cors({ origin: '*' }));
 app.use(express.json());
-
+const port =process.env.PORT || 5000;
 app.use('/contacts',contactRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log("MongoDB Connected");
-    app.listen(5000,()=>console.log("Server running on port 5000"));
+    app.listen(port,()=>console.log("Server running on port 5000"));
 })
 .catch((err)=>{
     console.log(err);
